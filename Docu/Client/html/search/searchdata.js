7c02520ad9ce0df4a4aa9c1da6a90697345795d9
Collignon Bastien <51114647+vRh4cking@users.noreply.github.com>
2021-03-14 17:42:25 +0100

    Add files via upload

---
+++ Docu/Client/html/search/searchdata.js
@@ -0,0 +1,24 @@
+var indexSectionsWithContent =
+{
+  0: "acdefghmoprstv~",
+  1: "cdefhmprstv",
+  2: "cdefpstv",
+  3: "acdefgoprstv~"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "files",
+  3: "functions"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Classes",
+  2: "Files",
+  3: "Functions"
+};
+
